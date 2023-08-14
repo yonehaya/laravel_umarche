a175946dc65219c652e31fbe91d78ff06f5036ac
はやと <104907508+yonehaya@users.noreply.github.com>
2023-08-15 00:32:39 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,5 +2,6 @@
   <x-slot name="header">ヘッダー1</x-slot>
   コンポーネントテスト1
   <x-tests.card title="タイトル1" content="本文1" :message="$message"></x-tests.card>
-  <x-tests.card title="タイトル1"></x-tests.card>
+  <x-tests.card title="タイトル2"></x-tests.card>
+  <x-tests.card title="CSS変更したい" class="bg-red-300"></x-tests.card>
 </x-tests.app>
