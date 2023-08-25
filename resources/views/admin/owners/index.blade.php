1114aa04b331f14b3b24a530c309da8ea0fe3832
はやと <104907508+yonehaya@users.noreply.github.com>
2023-08-26 00:44:07 +0900

    edit, update

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -11,7 +11,7 @@
         <div class="p-6 bg-white border-b border-gray-200">
           <section class="text-gray-600 body-font">
             <div class="container px-5 mx-auto">
-              <x-flash-message status="info"/>
+              <x-flash-message status="info" />
               <div class="flex justify-end lg:w-2/3 w-full mx-auto mb-4">
                 <button onclick="location.href='{{ route('admin.owners.create')}}'" class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">新規登録する</button>
               </div>
@@ -22,7 +22,7 @@
                       <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
                       <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メールアドレス</th>
                       <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">作成日</th>
-                      <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                      <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
                     </tr>
                   </thead>
                   <tbody>
@@ -31,8 +31,8 @@
                       <td class="px-4 py-3">{{$owner->name}}</td>
                       <td class="px-4 py-3"> {{$owner->email}}</td>
                       <td class="px-4 py-3 text-lg text-gray-900"> {{$owner->created_at->diffForHumans()}}</td>
-                      <td class="w-10 text-center">
-                        <input name="plan" type="radio">
+                      <td class="px-4 py-3 text-center">
+                        <button onclick="location.href='{{ route('admin.owners.edit', ['owner' => $owner->id ]) }}'" class="text-white bg-blue-400 border-0 py-2 px-4 focus:outline-none hover:bg-blue-600 rounded">編集</button>
                       </td>
                     </tr>
                     @endforeach
@@ -41,7 +41,6 @@
               </div>
             </div>
           </section>
-
           <!-- {{--  エロくアント
           @foreach ($e_all as $e_owner)
           {{$e_owner->name}}
