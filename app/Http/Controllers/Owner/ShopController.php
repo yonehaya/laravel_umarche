87da2565fc2189a2ee6f567fffe04b7ac09879a6
はやと <104907508+yonehaya@users.noreply.github.com>
2023-08-31 15:39:43 +0900

    index画面

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -32,9 +32,9 @@ class ShopController extends Controller
 
     public function index()
     {
-        $ownerId = Auth::id();
-        $shops = Shop::where('owner_id', $ownerId)->get();
+        $shops = Shop::where('owner_id', Auth::id())->get();
         return view('owner.shops.index', compact('shops'));
+
     }
 
     public function edit($id)
