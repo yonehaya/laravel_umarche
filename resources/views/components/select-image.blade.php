780c87d9717afa3176103d031eb64a980c53b0b2
はやと <104907508+yonehaya@users.noreply.github.com>
2023-09-09 15:59:39 +0900

    product create

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -3,6 +3,7 @@ if($name ==='image1'){$modal='modal-1';}
 if($name ==='image2'){$modal='modal-2';}
 if($name ==='image3'){$modal='modal-3';}
 if($name ==='image4'){$modal='modal-4';}
+if($name ==='image5'){$modal='modal-5';}
 
 
 @endphp
@@ -10,7 +11,7 @@ if($name ==='image4'){$modal='modal-4';}
 
 
 <div class="modal micromodal-slide" id="{{ $modal }}" aria-hidden="true">
-  <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+  <div class="modal__overlay z-50" tabindex="-1" data-micromodal-close>
     <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="{{ $modal }}-title">
       <header class="modal__header">
         <h2 class="modal__title text-xl text-gray-700" id="{{ $modal }}-title">
@@ -39,7 +40,7 @@ if($name ==='image4'){$modal='modal-4';}
   </div>
 </div>
 <div class="flex justify-around items-center mb-4">
-  <a data-micromodal-trigger="{{ $modal }}" href='javascript:;'>ファイルを選択</a>
+  <a class="py-2 px-4 bg-gray-200" data-micromodal-trigger="{{ $modal }}" href='javascript:;'>ファイルを選択</a>
   <div class="w-1/4">
     <img id="{{ $name }}_thumbnail" src="">
   </div>
